52fc73a2ee2ecb83b0362bb40abac9a39601cb85
Bhavya <bjain.nd@gmail.com>
2019-07-19 10:22:21 +0530

    Styling and API Content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox'
+// import { robots } from './robots';
+import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             SearchField: ''
         }
     }
@@ -17,13 +18,23 @@ onSearchChange = (event) => {
     
 }
 
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({robots:users}))
+}
+
     render() {
         const filterRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.SearchField.toLowerCase()) })
+        
 
+        if(this.state.robots.length === 0)
+        return <h1 className='dib tc'>Loading....</h1>
+        else 
         return (
             <div>
-                <h1 className='tc underline'>Robofriends</h1>
+                <h1 className='tc underline f1'>Robofriends</h1>
                 <SearchBox SearchChange = {this.onSearchChange}/>
                 < CardList robots={filterRobots} />
             </div>
